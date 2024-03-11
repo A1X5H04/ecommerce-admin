e9dd3378280537f628f547ee34a64719d88ceb48
Alish Baig <alishbaig2004@gmail.com>
2024-03-11 08:53:57 +0530

    feat: Add new files and utility functions

---
+++ lib/utils.ts
@@ -0,0 +1,17 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatDateTime(value: string) {
+  return new Date(value).toLocaleString();
+}
+
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
