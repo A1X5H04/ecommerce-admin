50e187e1eda17a6f82d9179936af952093bbf8f5
Alish Baig <alishbaig2004@gmail.com>
2024-03-11 08:51:34 +0530

    feat: Add store API routes for PATCH, DELETE, and POST methods

---
+++ app/api/stores/route.ts
@@ -0,0 +1,32 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { name } = await req.json();
+
+    console.log(name);
+
+    if (!name) {
+      return new NextResponse("Bad Request", { status: 400 });
+    }
+
+    const store = await prismadb.store.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
