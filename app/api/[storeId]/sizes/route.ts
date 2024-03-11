d1da3377f84d4f9603fc1b76ab8130f8a890c984
Alish Baig <alishbaig2004@gmail.com>
2024-03-11 08:51:22 +0530

    feat: Add size routes for CRUD operations

---
+++ app/api/[storeId]/sizes/route.ts
@@ -0,0 +1,71 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    const { name, value } = await req.json();
+
+    if (!name && !value) {
+      return new NextResponse("Bad Request", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("StoreId is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const size = await prismadb.size.create({
+      data: {
+        name,
+        value,
+
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("StoreId is required", { status: 400 });
+    }
+
+    const sizes = await prismadb.size.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(sizes);
+  } catch (error) {
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
