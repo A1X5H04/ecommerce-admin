a9a5905538fc50241e04c7ff8c9cae63d81589ef
Alish Baig <alishbaig2004@gmail.com>
2025-01-11 12:34:30 +0530

    feat: improve zod schema for category

--- lib/schema.ts
+++ lib/schema.ts
@@ -3,6 +3,14 @@ import * as z from "zod";
 export const categorySchema = z.object({
   name: z.string().min(3).max(255),
   description: z.string().min(10).max(255),
+  iconUrl: z.string().optional(),
+
+  metaTitle: z.string().optional(),
+  metaDescription: z.string().optional(),
+  metaKeywords: z.string().optional(),
+
+  status: z.boolean().default(true),
+
   parentCategoryId: z.string().optional(),
   type: z.string(),
 });
