27623c4b7caade6acf5ba34f97011488e52e864f
Alish Baig <alishbaig2004@gmail.com>
2025-01-11 12:35:01 +0530

    feat: add generate-slug and generate-session-token

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { encodeBase32LowerCaseNoPadding } from "@oslojs/encoding";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateSlug(text: string) {
+  return text
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/(^-|-$)+/g, "");
+}
+
+export function generateSessionToken() {
+  const randomBytes = crypto.getRandomValues(new Uint8Array(32));
+  return encodeBase32LowerCaseNoPadding(randomBytes);
 }
